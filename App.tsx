c13cac258d0667063668c068751e8fe96136f37b
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2024-03-11 03:35:24 -0300

    setup: added expo fonts roboto

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'expo-status-bar';
 import {Text, View } from 'react-native';
 
+
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
-    <View >
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{ backgroundColor: "#202024"}} >
+      <StatusBar style="light" translucent />
+      {fontsLoaded ? <Text>Open up App.tsx to start working on your app!</Text> : <View></View>}
+      
     </View>
   );
 }
