3db737beb03f36536913c9e5961af444e96291a5
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2024-03-12 22:42:33 -0300

    build: added nativebase dependency

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { StatusBar } from 'expo-status-bar';
-import {Text, View } from 'react-native';
-
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { StatusBar } from "expo-status-bar";
+import { NativeBaseProvider } from "native-base";
+import { Text, View } from "react-native";
 
 export default function App() {
-  const [ fontsLoaded ] = useFonts({
+  const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
-  })
+    Roboto_700Bold,
+  });
 
   return (
-    <View style={{ backgroundColor: "#202024"}} >
+    <NativeBaseProvider>
       <StatusBar style="light" translucent />
-      {fontsLoaded ? <Text>Open up App.tsx to start working on your app!</Text> : <View></View>}
-      
-    </View>
+      {fontsLoaded ? (
+        <Text>Open up App.tsx to start working on your app!</Text>
+      ) : (
+        <View></View>
+      )}
+    </NativeBaseProvider>
   );
 }
