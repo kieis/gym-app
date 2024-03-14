e5b045d1a27973524d6002b4faf9101af2728b4c
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2024-03-13 22:50:22 -0300

    feat: added header and cards to history screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,44 @@
-import { Center, Text } from "native-base";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Center, Heading, SectionList, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "26.08.22",
+      data: ["Data1", "Data2"],
+    },
+    {
+      title: "12.06.23",
+      data: ["Data1", "Data2"],
+    },
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text color="white">History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Exercises History" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard group="Chest" exercise={item} />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={
+          exercises.length === 0 && {
+            flex: 1,
+            justifyContent: "center",
+          }
+        }
+        ListEmptyComponent={() => (
+          <Text>There are no exercises registered today.</Text>
+        )}
+      />
+    </VStack>
   );
 }
