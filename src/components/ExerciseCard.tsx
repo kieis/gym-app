1ec5c65ca17bb649b97a5894ab3f88788721e353
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2024-03-13 22:31:43 -0300

    feat: added exercisecard component

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,39 @@
+import { HStack, Heading, Image, Text, VStack, Icon } from "native-base";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+interface Props extends TouchableOpacityProps {
+  name: string;
+  description: string;
+}
+
+export function ExerciseCard({ name, description, ...rest }: Props) {
+  return (
+    <TouchableOpacity {...rest}>
+      <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
+        <Image
+          source={{
+            uri: "https://static1.minhavida.com.br/articles/d6/99/d0/45/homem-fazendo-supino-inclinado-com-halteres-orig-1.jpg",
+          }}
+          alt="Exercise image"
+          w={16}
+          h={16}
+          rounded="md"
+          mr={4}
+          resizeMode="center"
+        />
+
+        <VStack flex={1}>
+          <Heading fontSize="lg" color="white">
+            {name}
+          </Heading>
+          <Text fontSize="sm" color="gray.200" mt={1} numberOfLines={2}>
+            {description}
+          </Text>
+        </VStack>
+
+        <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
+      </HStack>
+    </TouchableOpacity>
+  );
+}
