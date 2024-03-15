f5e829c8db1adb7b4f9a92bafcfe9fa149c614d2
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2024-03-15 16:40:51 -0300

    chore: added signout and loading with storage on auth context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
+import * as userStorage from "@storage/user";
 
 type SignInDataProps = {
   email: string;
@@ -10,6 +11,8 @@ type SignInDataProps = {
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (data: SignInDataProps) => Promise<void>;
+  signOut: () => Promise<void>;
+  isLoadingUserStorageData: boolean;
 };
 
 type AuthContextProviderProps = {
@@ -22,20 +25,55 @@ export const AuthContext = createContext<AuthContextDataProps>(
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
+  const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
+    useState(true);
 
   async function signIn({ email, password }: SignInDataProps) {
     try {
       const { data } = await api.post("/sessions", { email, password });
       if (data?.user) {
         setUser(data.user);
+        userStorage.set(data.user);
       }
     } catch (error) {
       throw error;
     }
   }
 
+  async function signOut() {
+    try {
+      setIsLoadingUserStorageData(true);
+      setUser({} as UserDTO);
+      userStorage.remove();
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
+  async function loadUserData() {
+    try {
+      const storageUser = await userStorage.get();
+      if (storageUser) {
+        setUser(storageUser);
+        setIsLoadingUserStorageData(false);
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider
+      value={{ user, signIn, signOut, isLoadingUserStorageData }}
+    >
       {children}
     </AuthContext.Provider>
   );
